1d703abd7bdbcd6df5ee8aa9a8e7f01bfb0af397
Aryanrathod21 <aryanrathod410@gmail.com>
2024-06-12 23:55:57 +0530

    cr7

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,13 +1,9 @@
-"use client"
+// src/components/banner/index.tsx
 import React, { ReactNode } from 'react';
 import videoSrc from '../../../public/onepiece.mp4';
-import { FaLongArrowAltRight } from 'react-icons/fa';
-import Link from 'next/link';
 
 type Props = {
   children: ReactNode;
-  imageSrc: string;
-  imageAlt: string;
 };
 
 const Banner = ({ children }: Props) => {
@@ -24,9 +20,9 @@ const Banner = ({ children }: Props) => {
         <div className='text-center p-4 bg-white text-black font-bold border-2 border-black rounded-lg shadow-chat-box'>
           {children}
         </div>
-       {/* <Link href="/Contact-page"><button className='mt-14 px-6 py-2 bg-green-500 text-white rounded-full hover:bg-red-600 flex items-center'>
+        {/* <Link href="/Contact-page"><button className='mt-14 px-6 py-2 bg-green-500 text-white rounded-full hover:bg-red-600 flex items-center'>
           Contact me <FaLongArrowAltRight className='ml-2' />
-        </button></Link>  */}
+        </button></Link> */}
       </div>
     </div>
   );
