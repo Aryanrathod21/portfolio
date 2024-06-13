7f72bacfa16e27e4df9ff918b1fc5d331a38a63b
Aryanrathod21 <aryanrathod410@gmail.com>
2024-06-13 14:30:03 +0530

    cr07si

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,4 +1,3 @@
-// src/components/banner/index.tsx
 import React, { ReactNode } from 'react';
 import videoSrc from '../../../public/onepiece.mp4';
 
@@ -9,12 +8,19 @@ type Props = {
 const Banner = ({ children }: Props) => {
   return (
     <div className='Banner relative h-screen'>
+      {/* Video for screens larger than md */}
       <video
         src={videoSrc}
         autoPlay
         muted
         loop
-        className='w-full h-full object-cover'
+        className='hidden md:block w-full h-full object-cover'
+      />
+      {/* Image for screens smaller than md */}
+      <img
+        src='/mobile.jpg'
+        alt='Banner Image'
+        className='block md:hidden w-full h-full object-cover'
       />
       <div className='absolute inset-0 flex flex-col items-center justify-center'>
         <div className='text-center p-4 bg-white text-black font-bold border-2 border-black rounded-lg shadow-chat-box'>
